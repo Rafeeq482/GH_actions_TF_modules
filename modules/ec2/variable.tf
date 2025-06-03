15e9cf10dc06c22a3f9a7bfc9e1782638a038f0f
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-03 12:28:03 +0530

    this is the new commit111

--- modules/ec2/variable.tf
+++ modules/ec2/variable.tf
@@ -7,7 +7,7 @@ variable "ami" {
 variable "instance_type" {
   type        = string
   description = "Type of EC2 instance"
-  default     = "automation-provisiong"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
